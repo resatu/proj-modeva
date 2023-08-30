dbc4d19ca3bef04435b5da26c616ff181e76438c
resatugurulu <resatugur.ulu@gmail.com>
2023-08-30 23:42:28 +0300

    submission

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,10 @@ const sass = require("sass");
 module.exports = function(eleventyConfig) {
   eleventyConfig.addTemplateFormats("scss");
   eleventyConfig.addPassthroughCopy("assets/scripts/**.*");
-
+  eleventyConfig.addPassthroughCopy("assets/images/**.*");
+  eleventyConfig.addPassthroughCopy("assets/fonts/**.*");
+  eleventyConfig.addPassthroughCopy("assets/icons/**.*");
+  eleventyConfig.addPassthroughCopy("assets", "assets");
   // Creates the extension for use
   eleventyConfig.addExtension("scss", {
     outputFileExtension: "css", // optional, default: "html"
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # proj-modeva
+
+<!-- some description about the project -->
+
+## Installation
+
+```bash
+# clone the project
+git clone
+
+# install dependencies
+npm install
+```
+
+
+<!-- how to install the project -->
+
+## Usage
+    
+    ```bash
+    # run the project
+    npm run dev
+    ```
+
+<!-- how to use the project -->
